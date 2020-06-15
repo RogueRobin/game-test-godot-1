516ff12017fffe9e975b2914ec63dd1f2dcdddfa
Azeeraphale <henriquemagalhaes95@hotmail.com>
2020-06-15 17:18:06 -0300

    Add files via upload

---
+++ testing/Player.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+const UP = Vector2(0, -1)
+var motion = Vector2()
+const GRAVITY = 20 
+const SPEED  = 200
+const JUMP_HEIGHT = -550
+
+func _physics_process(delta):
+	motion.y += GRAVITY
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$Sprite.play("run")
+		$Sprite.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$Sprite.play("run")
+		$Sprite.flip_h = true
+	else: 
+		motion.x = 0 
+		$Sprite.play("idle")					
+	if is_on_floor(): 
+		print("está no chão")
+		if Input.is_action_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+		
+	motion = move_and_slide(motion, UP)
